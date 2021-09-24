a96bac6aba33fad999ae58736306853b217d21ab
alicia <yutingyang@alicias-mbp.home>
2021-09-24 13:56:33 -0400

    Recitation5 demo

--- Recitation5/returnPointer.cpp
+++ Recitation5/returnPointer.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <array>
 
 // Buggy code
-int * count()
-{
-    int myInt = 5;
-
-    int * const p = &myInt;
-
-    return p;
+int* test () {
+   int c[5];
+   for (int i = 0; i < 5; i++) 
+          c[i] = i;
+   return c;
 }
+
 
 int main(){
-    std::cout << "Value is " << *count() << std::endl;
+    int * result = test();
+    std::cout << "Value is " << result[0] << std::endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Fix1
+// std::array<int,5> test () {
+//    std::array<int,5> c;
+//    for (int i = 0; i < 5; i++) 
+//           c[i] = i;
+//    return c;
+// }
+
+
+// int main(){
+//     std::array<int,5> result = test();
+//     std::cout << "First Value is " << result[0] << std::endl;
+// }
+
+
+// Fix2.
+// int* test () {
+//    int* c = new int[5];
+//    for (int i = 0; i < 5; i++) 
+//           c[i] = i;
+//    return c;
+// }
---
+++ Recitation5/stackheap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Test{
+
+}
+
+
+int main()
+{
+    int value = 5;  // stack allocation
+    int array[5];
+
+    int* hvalue = new int;     // heap allocation
+    *hvalue = 5;
+    int* harray = new int[5];
+
+}
